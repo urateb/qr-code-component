5efac77c6dd70bcb6bb714677b496d64d9d52e72
urateb <uratebekaj@gmail.com>
2023-12-13 21:26:21 +0100

    add finished qr code component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="qr-code__card">
+    <img src="./assets/images/image-qr-code.png" class="qr-code__image" />
+    <div class="qr-code__card--title">Improve your front-end skills by building projects</div>
+    <div class="qr-code__card--description">Scan the QR code to visit Frontend Mentor and take your coding skills to the
+      next level</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import './assets/styles/main.scss'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.qr-code {
+  &__card {
+    background-color: hsl(0, 0%, 100%);
+    border-radius: 16px;
+    width: 80%;
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 6px 8px 11px 1px hsla(218, 44%, 22%, 0.1);
+
+    &--title {
+      font-weight: 700;
+      text-align: center;
+      font-size: 21px;
+      margin: 30px 20px 20px 20px;
+    }
+
+    &--description {
+      text-align: center;
+      font-size: 15px;
+      color: hsl(220, 15%, 55%);
+      margin: 0 7px 20px 7px;
+
+    }
+
+    @media (min-width: 768px) {
+      width: 20%;
+    }
+  }
+
+  &__image {
+    width: 100%;
+    border-radius: 16px;
+  }
 }
 </style>
